6425e195d7e6fc70bbd955112f967c2bc60068f6
ashudhawas <ashudhawas05@gmail.com>
2024-10-15 12:00:52 +0530

    initial commit of xcode

---
+++ FromXcodeToGit/FromXcodeToGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  FromXcodeToGitApp.swift
+//  FromXcodeToGit
+//
+//  Created by Yug on 15/10/24.
+//
+
+import SwiftUI
+
+@main
+struct FromXcodeToGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FromXcodeToGitTests/FromXcodeToGitTests.swift
@@ -0,0 +1,16 @@
+//
+//  FromXcodeToGitTests.swift
+//  FromXcodeToGitTests
+//
+//  Created by Yug on 15/10/24.
+//
+
+import Testing
+
+struct FromXcodeToGitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
